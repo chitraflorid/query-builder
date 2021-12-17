6bedc6a9b577343ad39d5700435113d65e6bbcbf
chitra <chitraflorid@users.noreply.github.com>
2021-12-17 23:23:41 +0530

    Create index.js

---
+++ src/lib/common/components/enterpret-modal/index.js
@@ -0,0 +1,6 @@
+import { EnterpretModal } from './EnterpretModal';
+import { EnterpretModalHeader } from './EnterpretModalHeader';
+import { EnterpretModalBody } from './EnterpretModalBody';
+import {EnterpretModalFooter} from './EnterpretModalFooter';
+
+export {EnterpretModal, EnterpretModalHeader, EnterpretModalBody, EnterpretModalFooter };
